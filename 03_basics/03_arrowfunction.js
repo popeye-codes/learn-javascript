9b881392a8c675a8dd45e7ca91b9370aeca8f2e3
Shivam Raj <hey.shivam.uno@gmail.com>
2026-07-06 11:37:23 +0530

    ArrowFunction and this - keyword behaviour

--- 03_basics/03_arrowfunction.js
+++ 03_basics/03_arrowfunction.js
@@ -1,75 +1,99 @@
+// ====================== Objects & 'this' ======================
 
 const user = {
     username: "Sam",
     price: 999,
 
-    welcomeMessage: function() {
-        // this -> means current context , in this scope current value to access variable
-        console.log(`${this.username} , welcome to website`);
+    welcomeMessage: function () {
+        // 'this' refers to the current object that is calling the method.
+        console.log(`${this.username}, welcome to website`);
         console.log(this);
     }
-
-
-}
+};
 
 // user.welcomeMessage();
-// user.username = "John"; // we have change current - context (the value)
+
+// Changing the current object's property.
+user.username = "John";
+
 // user.welcomeMessage();
 
-// 🌟 when we access this keyword outside fn
-console.log(this); //🌟 as w're inside node-enviroment, this refers to Empty - object(i.e; {}), becoz now inside Global there is no-context
-// 🌟but if we run inside browser give window-object(as global object)
+
+// ====================== Global 'this' ======================
+
+// In Node.js, the global 'this' at the top level is an empty object {}.
+// In browsers, the global object is 'window'.
+console.log(this);
 
 
+// ====================== 'this' inside a Regular Function ======================
 
 // function chai() {
-//     // 🌟 when displaying just "this" inside fn gives so many values. console.log(this);
 //     let username = "shiv";
-//     console.log(this.username); // o/p -> undefined. even now not able to use this - keyword now
-// }
 
-// chai();  // ->🌟 we cant use this inside function
-
-
-
-// const chai = function () {
-//     let username = "shiv";
-//     console.log(this.username); // o/p -> undefined. same as Basc fn - declaration
+//     // 'this' exists inside a regular function,
+//     // but it does NOT refer to local variables.
+//     console.log(this);
+//     console.log(this.username); // undefined
 // }
 
 // chai();
 
 
+// ====================== Function Expression ======================
 
-//🌟 Arraow function
-const chia = () => {
+// const chai = function () {
+//     let username = "shiv";
+
+//     // Same behavior as a regular function.
+//     console.log(this);
+//     console.log(this.username); // undefined
+// };
+
+// chai();
+
+
+// ====================== Arrow Function ======================
+
+const chai = () => {
     let username = "shiv";
 
-    //🌟 here also same behaviour as above
+    // Arrow functions do NOT have their own 'this'.
+    // They inherit 'this' from the surrounding (lexical) scope.
     console.log(this);
-    console.log(this.username);  
-}
+    console.log(this.username); // undefined
+};
 
-chia();
+chai();
 
 
-// ---- 🌟 basic - arrow function
-// Explicit - return
+// ====================== Arrow Functions ======================
+
+// Explicit Return
 const addTwo = (num1, num2) => {
     return num1 + num2;
-} 
-console.log(addTwo(3,2));
+};
+
+console.log(addTwo(3, 2));
 
 
-// ---- 🌟 arraw func with implicit - return
-const add3 = (num1, num2) =>  num1 + num2;
+// Implicit Return
 
-// const add3 = (num1, num2) =>  (num1 + num2); // ⚠️🌟 wrapped in parenthesis means no need of return - keyword
+const addThree = (num1, num2) => num1 + num2;
 
-console.log(add3(8, 9));
+// Parentheses also perform an implicit return.
+// const addThree = (num1, num2) => (num1 + num2);
+
+console.log(addThree(8, 9));
 
 
-// 🌟🌟🌟 implicit return -> useful to return object
-const returnObject = () => ({username: "Raghu"});
+// ====================== Returning an Object ======================
+
+// Wrap the object in parentheses.
+// Otherwise '{}' is treated as the function body.
+
+const returnObject = () => ({
+    username: "Raghu"
+});
 
 console.log(returnObject());
