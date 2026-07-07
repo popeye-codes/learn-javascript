07ca5539cb2a9ba52e1f031c4fd783324610bd8e
Shivam Raj <hey.shivam.uno@gmail.com>
2026-07-07 13:14:53 +0530

    Loops - in JavaScript

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+// while - loop
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(`value of i is: ${i}`);
+
+//     i = i + 2;
+// }
+
+
+// do - while 
+
+let score = 11;
+
+do {
+    console.log(`Score is: ${score}`);
+    score++;
+} while (score <= 10);
